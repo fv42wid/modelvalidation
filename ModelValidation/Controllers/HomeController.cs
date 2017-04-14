c300f3b04866be85fba94b45091d95e345a929b5
Frank Vinci <fv42wid@yahoo.com>
2017-04-14 20:12:19 +0000

    finish chapter 27

---
+++ ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModelValidation.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() =>
+            View("MakeBooking", new Appointment { Date = DateTime.Now });
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt)
+        {
+            if(ModelState.GetValidationState(nameof(appt.Date)) == ModelValidationState.Valid
+                && appt.ClientName.Equals("Joe", StringComparison.OrdinalIgnoreCase)
+                && appt.Date.DayOfWeek == DayOfWeek.Monday)
+            {
+                ModelState.AddModelError("", "Joe cannot book appointments on Mondays");
+            }
+            if(ModelState.IsValid)
+            {
+                return View("Completed", appt);
+            } else
+            {
+                return View();
+            }
+        }
+
+        public JsonResult ValidateDate(string Date)
+        {
+            DateTime parsedDate;
+            return Json("this works");
+            /*if(!DateTime.TryParse(Date, out parsedDate))
+            {
+                return Json("Please enter a valid date (mm/dd/yyyy)");
+            } else if(DateTime.Now > parsedDate)
+            {
+                return Json("Please enter a date in the future");
+            } else
+            {
+                return Json(true);
+            }*/
+        }
+    }
+}
